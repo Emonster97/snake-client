16c6ad2625dcedce763b93b7fc2fe26e6a11076e
Wilfred <wilfredemonts@gmail.com>
2021-12-01 14:20:44 -0500

    server message added

--- client.js
+++ client.js
@@ -6,6 +6,13 @@ const connect = function () {
   const conn = net.createConnection({
     host: '165.227.47.243', // IP address here,
     port: 50541 // PORT number here,
+    
+  });
+  conn.on('connect', (data) => {
+    console.log("Successfully connected to game server");
+  })
+  conn.on('connect', () => {
+    conn.write('Name: WME');
   });
   conn.on('data', (data) => {
     console.log('you ded cuz you idled');
